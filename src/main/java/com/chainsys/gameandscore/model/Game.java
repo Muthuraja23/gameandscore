3941477db5c0272d1d8d9991861db5a1636a527f
Muthuraja23 <muthuraja.alaguseeman@chainsys.com>
2022-08-14 15:08:50 +0530

    sonar issue

--- src/main/java/com/chainsys/gameandscore/model/Game.java
+++ src/main/java/com/chainsys/gameandscore/model/Game.java
@@ -4,9 +4,7 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
