fa502bdb9120ef78511978f2762f339f4182d1f7
Muthuraja23 <muthuraja.alaguseeman@chainsys.com>
2022-08-19 09:52:21 +0530

    sonar issue

--- src/main/java/com/chainsys/gameandscore/businesslogic/Logic.java
+++ src/main/java/com/chainsys/gameandscore/businesslogic/Logic.java
@@ -3,7 +3,8 @@ package com.chainsys.gameandscore.businesslogic;
 import java.time.LocalDate;
 
 public class Logic {
-
+private Logic() {
+}
 	public static LocalDate getInstanceDate() {
         return java.time.LocalDate.now();
     }
