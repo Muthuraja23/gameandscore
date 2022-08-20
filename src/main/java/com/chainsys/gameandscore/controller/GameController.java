ad0a7a6c05d24ebe94097ead3bb438de481950c8
Muthuraja23 <muthuraja.alaguseeman@chainsys.com>
2022-08-20 09:33:54 +0530

    sonar issue

--- src/main/java/com/chainsys/gameandscore/controller/GameController.java
+++ src/main/java/com/chainsys/gameandscore/controller/GameController.java
@@ -19,57 +19,55 @@ import com.chainsys.gameandscore.service.GameService;
 @RequestMapping("/game")
 public class GameController {
 	public static final String LISTOFGAMES = "redirect:/game/getallgames";
-    public static final String ADDGAME = "add-game-form";
-    public static final String UPDATEGAME = "update-game-form";
 	@Autowired
 	private GameService gmservice;
+
 	@GetMapping("/index")
 	public String index() {
-		
+
 		return "index";
 	}
 
 	@GetMapping("/getallgames")
-	public String getallgames(@RequestParam("id")int sportsId,Model model) {
+	public String getallgames(@RequestParam("id") int sportsId, Model model) {
 		List<Game> gamelist = gmservice.getGame(sportsId);
 		model.addAttribute("allgames", gamelist);
 		return "game-list";
 	}
+
 	@GetMapping("/directioncontroll")
-	public String directionScoreBoard(@RequestParam("id")int gameId,Model model) {
-		Game game=gmservice.findById(gameId);
-		if(game.getSportsid()==1) {
-			return "redirect:/cricket/getscore?id="+gameId;
-		}
-		else if(game.getSportsid()==2) {
-			return "redirect:/football/getscore?id="+gameId;
-		}
-		else if(game.getSportsid()==3) {
-			return "redirect:/hockey/getscore?id="+gameId;
-		}
-		else {
-			return "redirect:/game/getallgames?id="+gameId;
+	public String directionScoreBoard(@RequestParam("id") int gameId, Model model) {
+		Game game = gmservice.findById(gameId);
+		if (game.getSportsid() == 1) {
+			return "redirect:/cricket/getscore?id=" + gameId;
+		} else if (game.getSportsid() == 2) {
+			return "redirect:/football/getscore?id=" + gameId;
+		} else if (game.getSportsid() == 3) {
+			return "redirect:/hockey/getscore?id=" + gameId;
+		} else {
+			return "redirect:/game/getallgames?id=" + gameId;
 		}
 	}
+
 	@GetMapping("addform")
 	public String gameAddForm(Model model) {
 		Game g = new Game();
 		model.addAttribute("addgame", g);
-		return ADDGAME;
+		return "add-game-form";
 	}
 
 	@PostMapping("/add")
 	public String addnewsports(@ModelAttribute("addgame") Game gm) {
 		gmservice.save(gm);
-		int id=gm.getSportsid();
-		return "redirect:/game/getallgames?id="+id;
+		int id = gm.getSportsid();
+		return "redirect:/game/getallgames?id=" + id;
 	}
 
 	@GetMapping("/updateform")
 	public String showUpdateForm(@RequestParam("gameid") int id, Model model) {
 		Game g = gmservice.findById(id);
 		model.addAttribute("updategame", g);
-		return UPDATEGAME;
+		return "update-game-form";
 	}
 
 	@PostMapping("/updategame")
@@ -81,13 +79,13 @@ public class GameController {
 	@GetMapping("/deletegame")
 	public String deleteGame(@RequestParam("gameid") int id) {
 		gmservice.deleteById(id);
-		return "Deleted";
+		return LISTOFGAMES;
 	}
 
 	@GetMapping("/getfootballscoreboard")
 	public String getGamScoreBoard(@RequestParam("id") int id, Model model) {
-		GameFootBallDTO  dto = gmservice.getGameFootBall(id);
-		model.addAttribute("getGame",dto.getGame());
+		GameFootBallDTO dto = gmservice.getGameFootBall(id);
+		model.addAttribute("getGame", dto.getGame());
 		model.addAttribute("getScoreboardlist", dto.getFootballScoreboards());
 		return "game-scoreboard-list";
 	}
